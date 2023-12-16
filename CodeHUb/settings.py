edce1e64d8d57ca85888620897ec79b41d1d7ad3
talasbek <algazievtalas@gmail.com>
2023-12-16 23:49:05 +0600

    add pagination to API(products)

--- CodeHUb/settings.py
+++ CodeHUb/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
@@ -15,6 +16,7 @@ class CategoryAPIView(ListAPIView):
 class ProductAPIView(ListAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
+    pagination_class = PageNumberPagination
 
 
 class OrderAPIView(APIView):
