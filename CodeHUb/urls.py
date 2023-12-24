4d460642fef24b6b264b964a6ba88033b995ef45
talasbek <algazievtalas@gmail.com>
2023-12-25 00:11:22 +0600

    successful deploy PROJECT REST API

--- CodeHUb/urls.py
+++ CodeHUb/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/schema/redooc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
